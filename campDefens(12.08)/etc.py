22cf356e5917b8f0be0d5ddc044b985a9be95d88
유승현 <you2861@naver.com>
2015-12-08 00:56:39 +0900

    스테이지 증설(기존1개 + 신규 4개)
    기존의 1개 스테이지 -> 총 5개의 스테이지 (밸런스 X)

---
+++ campDefens(12.08)/etc.py
@@ -0,0 +1,121 @@
+from pico2d import*
+import game_framework
+import end_state
+
+class Clock:
+    def __init__(self):
+        self.image = load_image('image\\etc\\number.png')
+        self.colon = load_image('image\\etc\\timer1.png')
+        self.hx = 480
+        self.m1x,self.m2x = 520, 540
+        self.y = 550
+        self.time = 60
+        self.time_over = False
+
+    def update(self, frame_time):
+        self.time -= frame_time
+        if self.time < 1:
+            self.time_over = True
+
+    def draw(self):
+        self.image.clip_draw(23 * (int(self.time / 60)), 0, 23, 34, self.hx, self.y)
+        self.image.clip_draw(23 * (int(self.time % 60 / 10)), 0, 23, 34, self.m1x, self.y)
+        self.image.clip_draw(23 * (int(self.time) % 10), 0, 23, 34, self.m2x, self.y)
+        self.colon.clip_draw(0,0,10,24,500,550,20,24)
+
+class Score:
+    def __init__(self):
+        self.image = load_image('image\\etc\\score_board.png')
+        self.text = load_font('font\\arial.ttf', 30)
+        self.score = 0
+        self.frame = 0
+
+    def update(self):
+        if self.score < 2000: self.frame = 0
+        if self.score >= 2000: self.frame = 1
+        if self.score >= 4000: self.frame = 2
+        if self.score >= 6000: self.frame = 3
+        if self.score >= 8000: self.frame = 4
+        if self.score >= 10000: self.frame = 5
+        if self.score >= 12500: self.frame = 6
+        if self.score >= 15000: self.frame = 7
+
+    def draw(self):
+        self.image.clip_draw(100 * self.frame,0,100,19,150,500, 300, 50)
+        self.text.draw(180,500,("%d") % self.score, color = (255,255,255))
+
+class Life:
+    def __init__(self):
+        self.life = 20
+        self.image = load_image('image\\etc\\Life.png')
+        self.font = load_font('font\\arial.ttf', 25)
+
+    def update(self):
+        if self.life <= 0:
+            game_framework.push_state(end_state)
+
+    def draw(self):
+        self.image.draw(50,550)
+        self.font.draw(80, 550, 'X %d'%self.life, color = (255,0,0))
+
+class Buff:
+    speed_normal, speed_up, speed_down = 1, 2, 3
+    hp_normal, hp_up, hp_down = 1, 2, 3
+
+    def handle_speed_normal(self, objects, frame_time):
+        for object in objects:
+            object.speed_state = object.normal
+
+    def handle_speed_up(self, objects, frame_time):
+        self.speed_buff_time += frame_time
+        for object in objects:
+            object.speed_state = object.speed_up
+        if self.speed_buff_time > 2:
+            self.speed_buff = self.speed_normal
+            self.speed_buff_time = 0
+
+    def handle_speed_down(self, objects, frame_time):
+        self.speed_buff_time += frame_time
+        for object in objects:
+            object.speed_state = object.speed_down
+        if self.speed_buff_time > 2:
+            self.speed_buff = self.speed_normal
+            self.speed_buff_time = 0
+
+    speed_state_tabel = {
+        speed_normal : handle_speed_normal,
+        speed_up : handle_speed_up,
+        speed_down : handle_speed_down
+    }
+
+    def handle_hp_normal(self, objects):
+        pass
+
+    def handle_hp_up(self, objects):
+        for object in objects:
+            object.hp += 1
+        self.hp_buff = self.hp_normal
+
+    def handle_hp_down(self, objects):
+        for object in objects:
+            if object.hp > 1:
+                object.hp -= 1
+        self.hp_buff = self.hp_normal
+
+    hp_state_tabel = {
+        speed_normal : handle_hp_normal,
+        speed_up : handle_hp_up,
+        speed_down : handle_hp_down
+    }
+
+    def __init__(self):
+        self.speed_buff = Buff.speed_normal
+        self.hp_buff = Buff.hp_normal
+        self.speed_buff_time = 0.0
+
+    def update(self, frame_time, objects):
+        self.speed_state_tabel[self.speed_buff](self,objects, frame_time)
+        self.hp_state_tabel[self.hp_buff](self,objects)
+
+    def drow(self):
+        pass
---
+++ campDefens(12.08)/fifth_stage.py
@@ -0,0 +1,190 @@
+from pico2d import *
+import random
+import game_framework
+import fourth_stage
+import clear_state
+import end_state
+import background
+import monster
+import friendly
+import weapon
+import etc
+
+total_time = 0.0
+main_background = None
+weather = None
+timer = None
+score = None
+Life = None
+buff = None
+Bouns_monster = None
+Heroine = None
+monstertype = []
+friendlytype = []
+select = []
+ballistas = []
+arrows = []
+
+def enter():
+    global main_background, timer, score, Life, buff
+    global monstertype, select, friendlytype, Bouns_monster, Heroine
+    global ballistas, arrows
+    main_background = background.Background()
+    monstertype = [monster.Falcon, monster.Ice_wolf, monster.Yeti]
+    friendlytype = [friendly.Amazon, friendly.General, friendly.Soldier]
+    Bouns_monster = monster.Bonus()
+    Heroine = friendly.Heroine()
+    ballistas = [weapon.Weapon(i) for i in range(4)]
+    timer = etc.Clock()
+    score = etc.Score()
+    Life = fourth_stage.Life
+    buff = etc.Buff()
+    select = []
+    arrows = []
+    monster.stage_set(Life, score)
+    friendly.stage_set(Life, score)
+    weapon.stage_set(select)
+
+def exit():
+    global main_background, timer, score, Life, weather
+    global monstertype, friendlytype, select
+    global ballistas, arrows
+    del(main_background)
+    if weather != None:
+        del(weather)
+    del(timer)
+    del(score)
+    del(Life)
+    del(monstertype)
+    del(friendlytype)
+    del(select)
+    del(ballistas)
+    del(arrows)
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+def handle_events(frame_time):
+    global testing
+    global arrows
+    global score, weather
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.quit()
+            if event.key == SDLK_1 and ballistas[0].state == ballistas[0].READY:
+                arrows.append(weapon.Arrow(110, 280))
+                ballistas[0].state = ballistas[0].LAUNCH
+            if event.key == SDLK_2 and ballistas[1].state == ballistas[1].READY:
+                arrows.append(weapon.Arrow(210, 230))
+                ballistas[1].state = ballistas[1].LAUNCH
+            if event.key == SDLK_3 and ballistas[2].state == ballistas[2].READY:
+                arrows.append(weapon.Arrow(310, 180))
+                ballistas[2].state = ballistas[2].LAUNCH
+            if event.key == SDLK_4 and ballistas[3].state == ballistas[3].READY:
+                arrows.append(weapon.Arrow(410, 130))
+                ballistas[3].state = ballistas[3].LAUNCH
+            if event.key == SDLK_5:
+                weather = None
+            if event.key == SDLK_6:
+                weather = background.Snow()
+            if event.key == SDLK_7:
+                weather = background.Rain()
+            if event.key == SDLK_u:
+                Life.life -= 20
+            if event.key == SDLK_b:
+                score.score -= 1000
+
+def update(frame_time):
+    main_background.update(frame_time)
+    if weather != None:
+        weather.update(frame_time)
+    score.update()
+    create_object(frame_time, select)
+    for mon in select:
+        mon.update(frame_time)
+    for i in range(4):
+        ballistas[i].update(frame_time)
+    for arrow in arrows:
+        arrow.update(frame_time)
+    delete_object(select)
+    delete_object(arrows)
+    buff.update(frame_time, select)
+    Life.update()
+    timer.update(frame_time)
+
+def draw(frame_time):
+    clear_canvas()
+    main_background.draw()
+    if weather != None:
+        weather.draw()
+    timer.draw()
+    score.draw()
+    Life.draw()
+    for mon in select:
+        mon.draw()
+    for i in range(0,4):
+        ballistas[i].draw()
+    for arrow in arrows:
+        arrow.draw()
+
+    update_canvas()
+
+def create_object(frame_time, objects):
+    global total_time
+    total_time += frame_time
+    if total_time > (random.randint(20, 30) * 0.1):
+        #if 'Bouns' not in objects.name: objects.append(monster.Bonus())
+        #if 'Heroine' not in objects.name: objects.append(friendly.Heroine())
+        if random.randint(0,2) : objects.append(monstertype[random.randint(0,2)]())
+        else : objects.append(friendlytype[random.randint(0,2)]())
+        total_time = 0.0
+
+def delete_object(objects):
+    for object in objects:
+        if object.alive == False:
+            if object.name == 'Bouns':
+                objects.append(monster.Bonus())
+                if object.hp < 1:
+                    if random.randint(0,1):
+                        if buff.speed_buff == buff.speed_normal:
+                            if random.randint(0,1) : speed_up(buff)
+                            else : speed_down(buff)
+                    else:
+                        if buff.hp_buff == buff.hp_normal:
+                            if random.randint(0,1) : hp_up(buff)
+                            else : hp_down(buff)
+
+            if object.name == 'Heroine':
+                objects.append(friendly.Heroine())
+
+            objects.remove(object)
+
+def game_end(time):
+    if time.time_over == True:
+        if score >= 5000 :
+            game_framework.push_state(clear_state)
+        else:
+            game_framework.push_state(end_state)
+
+def speed_up(buff):
+    buff.speed_buff = buff.speed_up
+    print('speed_up! \n')
+
+def speed_down(buff):
+    buff.speed_buff = buff.speed_up
+    print('speed_down! \n')
+
+def hp_up(buff):
+    buff.hp_buff = buff.hp_up
+    print('hp_up! \n')
+
+def hp_down(buff):
+    buff.hp_buff = buff.hp_down
+    print('hp_down! \n')
