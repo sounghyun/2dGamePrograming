210f9a79dcb0392953483c9fe1e6bbcfcf0a1eaa
유승현 <you2861@naver.com>
2015-12-11 00:21:35 +0900

    스테이지별 밸런스 조절, 날씨 설정
    스테이지별 출현 몬스터 및 클리어점수 조절, 특정 스테이지 날씨 출현(시야방해)

---
+++ campDefens(12.11)/end_state.py
@@ -0,0 +1,61 @@
+import game_framework
+import title_state
+from pico2d import *
+
+
+name = "StartState"
+image = None
+bgm = None
+logo_time = 0.0
+
+
+def enter():
+    global image
+    global bgm
+    image = load_image('image\\ending_background.png')
+    bgm = load_music('sound\\ending.mp3')
+    bgm.set_volume(50)
+    bgm.repeat_play()
+
+def exit():
+    global image
+    global bgm
+    del(image)
+    del(bgm)
+
+def update(frame_time):
+    global logo_time
+
+    if(logo_time > 5.0):
+        logo_time = 0
+        game_framework.push_state(title_state)
+    logo_time += frame_time
+
+
+def draw(frame_time):
+    global image
+    clear_canvas()
+    image.draw(500, 300, 1000, 600)
+    update_canvas()
+
+
+
+
+def handle_events(frame_time):
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+
+
+def pause(): pass
+
+
+def resume(): pass
+
+
+
+
